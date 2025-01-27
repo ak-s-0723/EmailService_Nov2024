fc86f4cd92f10b9c9eacff009000a7a7d2d2d67b
khannaanurag1 <khannaanurag1@gmail.com>
2025-01-27 09:09:21 +0530

    send email

---
+++ src/main/java/org/example/emailservice_nov2024/dtos/EmailDto.java
@@ -0,0 +1,13 @@
+package org.example.emailservice_nov2024.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class EmailDto {
+    private String to;
+    private String from;
+    private String subject;
+    private String body;
+}
